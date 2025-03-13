d9e89abb551845a844645f03565e1ecf680109af
fctico11 <fac322@nyu.edu>
2025-03-13 17:00:41 -0600

    adjusted navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
+import "./Navbar.css";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { FiMenu, FiX } from "react-icons/fi"; // Import icons
-import "./Navbar.css";
+import { FiMenu, FiX } from "react-icons/fi"; 
+
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,33 +29,31 @@ const Navbar = () => {
         isScrolled ? "bg-black shadow-lg" : "bg-transparent"
       }`}
     >
-      {/* Logo */}
+      {/* Logo with Custom Font */}
       <div className="text-xl md:text-3xl font-bold text-yellow-500 clinking-font">
         Clinking Bubbles Co.
       </div>
 
       {/* Desktop Links */}
-      <div className="hidden md:flex space-x-8">
-        <Link
-          to="/"
-          className="text-yellow-500 relative group transition"
-        >
+      <div className="hidden md:flex space-x-8 items-center">
+        <Link to="/" className="text-yellow-500 relative group transition">
           Home
           <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-yellow-500 transition-all duration-300 group-hover:w-full"></span>
         </Link>
-        <Link
-          to="/about"
-          className="text-yellow-500 relative group transition"
-        >
+        <Link to="/about" className="text-yellow-500 relative group transition">
           About
           <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-yellow-500 transition-all duration-300 group-hover:w-full"></span>
         </Link>
-        <Link
-          to="/contact"
-          className="text-yellow-500 relative group transition"
-        >
-          Contact
+        <Link to="/alcohol-calculator" className="text-yellow-500 relative group transition">
+          Alcohol Calculator
           <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-yellow-500 transition-all duration-300 group-hover:w-full"></span>
+        </Link>
+
+        {/* Get a Quote Button */}
+        <Link to="/contact">
+          <button className="bg-yellow-500 text-black font-semibold px-5 py-2 rounded-lg shadow-md hover:bg-yellow-600 transition">
+            Get a Quote!
+          </button>
         </Link>
       </div>
 
@@ -65,7 +64,7 @@ const Navbar = () => {
         </button>
       </div>
 
-      {/* Mobile Menu Overlay (Small & Non-Fullscreen) */}
+      {/* Mobile Menu Overlay (Small Dropdown) */}
       {isOpen && (
         <div className="fixed top-14 right-4 w-48 bg-black border border-gray-700 rounded-lg shadow-lg p-4 flex flex-col space-y-3 md:hidden">
           <Link to="/" className="text-yellow-500 hover:text-white transition" onClick={toggleMenu}>
@@ -74,8 +73,15 @@ const Navbar = () => {
           <Link to="/about" className="text-yellow-500 hover:text-white transition" onClick={toggleMenu}>
             About
           </Link>
-          <Link to="/contact" className="text-yellow-500 hover:text-white transition" onClick={toggleMenu}>
-            Contact
+          <Link to="/alcohol-calculator" className="text-yellow-500 hover:text-white transition" onClick={toggleMenu}>
+            Alcohol Calculator
+          </Link>
+
+          {/* Get a Quote Button in Mobile Menu */}
+          <Link to="/contact">
+            <button className="w-full bg-yellow-500 text-black font-semibold px-4 py-2 rounded-lg shadow-md hover:bg-yellow-600 transition">
+              Get a Quote!
+            </button>
           </Link>
         </div>
       )}
@@ -84,4 +90,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
