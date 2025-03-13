dffeeb638ab940579347ac59041ec23953da0f0c
fctico11 <fac322@nyu.edu>
2025-03-13 16:32:21 -0600

    changed font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 // src/components/Navbar.js
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="bg-black text-white p-4 flex justify-between items-center">
-      <div className="text-2xl font-bold text-yellow-500">Clinking Bubbles Co.</div>
+      <div className="text-2xl font-bold text-yellow-500 clinking-font">
+        Clinking Bubbles Co.
+      </div>
       <div className="space-x-4">
         <Link to="/" className="hover:text-yellow-500">Home</Link>
         <Link to="/about" className="hover:text-yellow-500">About</Link>
