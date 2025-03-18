0b421cc504b5ba77c70d131b42d615ed33dc2230
fctico11 <fac322@nyu.edu>
2025-03-17 20:09:12 -0600

    update to champagne icon

--- src/components/Services.js
+++ src/components/Services.js
@@ -10,43 +10,43 @@ import corporateIcon from "../assets/icons/handshake.png";
 import birthdayIcon from "../assets/icons/birthday-cake.png";
 import graduationIcon from "../assets/icons/graduate.png";
 import anniversaryIcon from "../assets/icons/anniversary.png";
-import moreIcon from "../assets/icons/more.png";
+import champagneIcon from "../assets/icons/champagne.png";
 
 const servicesData = [
   {
     title: "Private Parties",
-    description: "Personalized bartending services for an intimate, memorable event.",
+    description: "Personalized bartending services for an intimate, memorable event",
     icon: privateIcon,
   },
   {
     title: "Corporate Events",
-    description: "Elevate your corporate gatherings with tailored cocktail experiences.",
+    description: "Elevate your corporate gatherings with tailored cocktail experiences",
     icon: corporateIcon,
   },
   {
     title: "Weddings",
-    description: "Add a touch of luxury to your special day with bespoke cocktail menus.",
+    description: "Add a touch of luxury to your special day with bespoke cocktail menus",
     icon: ringsIcon,
   },
   {
     title: "Birthday Parties",
-    description: "Celebrate in style with custom cocktails and dedicated bartenders.",
+    description: "Celebrate in style with custom cocktails and dedicated bartenders",
     icon: birthdayIcon,
   },
   {
     title: "Graduation Parties",
-    description: "Make the milestone unforgettable with our signature drinks.",
+    description: "Make the milestone unforgettable with our signature drinks",
     icon: graduationIcon,
   },
   {
     title: "Anniversaries",
-    description: "Toast to lasting love with a refined cocktail experience.",
+    description: "Toast to lasting love with a refined cocktail experience",
     icon: anniversaryIcon,
   },
   {
     title: "So Much More...",
     description: "No matter the occasion, our team is dedicated to tailoring an experience that exceeds expectations every time",
-    icon: moreIcon,
+    icon: champagneIcon,
   },
   // Add more services if desired
 ];
