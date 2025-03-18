a8f0a23362e357d693395175378115b511376a96
fctico11 <fac322@nyu.edu>
2025-03-17 20:56:41 -0600

    removed smooth scroll and made direct jump navigation

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -4,12 +4,12 @@ import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
-  
+
   useEffect(() => {
-    // Scroll to the top of the page when the pathname changes
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    // Immediately jump to top (no smooth scrolling)
+    window.scrollTo(0, 0);
   }, [pathname]);
-  
+
   return null;
 };
 
