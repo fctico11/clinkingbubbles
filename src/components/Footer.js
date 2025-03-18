8f167209ae597616cd250632d4d00935e4925717
fctico11 <fac322@nyu.edu>
2025-03-18 12:24:51 -0600

    added link resource

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaInstagram, FaEnvelope } from "react-icons/fa"; 
+import { FaInstagram, FaEnvelope, FaExternalLinkAlt } from "react-icons/fa"; 
 import { SiTiktok } from "react-icons/si";
 import { Link } from "react-router-dom";
 
@@ -17,84 +17,84 @@ const Footer = () => {
         </div>
 
       {/* Social Icons */}
-<div className="flex justify-center space-x-6 mb-6">
-  {/* Instagram */}
-  <a
-    href="https://www.instagram.com/clinkingbubbles"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <div className="relative inline-block w-[30px] h-[30px]">
-      {/* Black "border" layer (slightly larger) */}
-      <FaInstagram
-        size={28}
-        className="absolute top-1/2 left-1/2 
-                   transform -translate-x-1/2 -translate-y-1/2
-                   text-black"
-      />
-      <FaInstagram
-        size={24}
-        className="absolute top-1/2 left-1/2 
-                   transform -translate-x-1/2 -translate-y-1/2
-                   text-black"
-      />
-      {/* White (or your preferred color) icon on top */}
-      <FaInstagram
-        size={26}
-        className="absolute top-1/2 left-1/2 
-                   transform -translate-x-1/2 -translate-y-1/2
-                   text-white hover:text-yellow-500 transition"
-      />
-    </div>
-  </a>
+        <div className="flex justify-center space-x-6 mb-6">
+          {/* Instagram */}
+          <a
+            href="https://www.instagram.com/clinkingbubbles"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="relative inline-block w-[30px] h-[30px]">
+              {/* Black "border" layer (slightly larger) */}
+              <FaInstagram
+                size={28}
+                className="absolute top-1/2 left-1/2 
+                          transform -translate-x-1/2 -translate-y-1/2
+                          text-black"
+              />
+              <FaInstagram
+                size={24}
+                className="absolute top-1/2 left-1/2 
+                          transform -translate-x-1/2 -translate-y-1/2
+                          text-black"
+              />
+              {/* White (or your preferred color) icon on top */}
+              <FaInstagram
+                size={26}
+                className="absolute top-1/2 left-1/2 
+                          transform -translate-x-1/2 -translate-y-1/2
+                          text-white hover:text-yellow-500 transition"
+              />
+            </div>
+          </a>
 
-  {/* TikTok */}
-  <a
-    href="https://www.tiktok.com/@clinkingbubbles"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <div className="relative inline-block w-[30px] h-[30px]">
-      <SiTiktok
-        size={30}
-        className="absolute top-1/2 left-1/2
-                   transform -translate-x-1/2 -translate-y-1/2
-                   text-black"
-      />
-      <SiTiktok
-        size={20}
-        className="absolute top-1/2 left-1/2
-                   transform -translate-x-1/2 -translate-y-1/2
-                   text-black"
-      />
-      <SiTiktok
-        size={26}
-        className="absolute top-1/2 left-1/2
-                   transform -translate-x-1/2 -translate-y-1/2
-                   text-white hover:text-yellow-500 transition"
-      />
-      
-    </div>
-  </a>
+          {/* TikTok */}
+          <a
+            href="https://www.tiktok.com/@clinkingbubbles"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="relative inline-block w-[30px] h-[30px]">
+              <SiTiktok
+                size={30}
+                className="absolute top-1/2 left-1/2
+                          transform -translate-x-1/2 -translate-y-1/2
+                          text-black"
+              />
+              <SiTiktok
+                size={20}
+                className="absolute top-1/2 left-1/2
+                          transform -translate-x-1/2 -translate-y-1/2
+                          text-black"
+              />
+              <SiTiktok
+                size={26}
+                className="absolute top-1/2 left-1/2
+                          transform -translate-x-1/2 -translate-y-1/2
+                          text-white hover:text-yellow-500 transition"
+              />
+              
+            </div>
+          </a>
 
-  {/* Envelope */}
-  <Link to="/contact">
-    <div className="relative inline-block w-[30px] h-[30px]">
-      <FaEnvelope
-        size={30}
-        className="absolute top-1/2 left-1/2
-                   transform -translate-x-1/2 -translate-y-1/2
-                   text-black"
-      />
-      <FaEnvelope
-        size={26}
-        className="absolute top-1/2 left-1/2
-                   transform -translate-x-1/2 -translate-y-1/2
-                   text-white hover:text-yellow-500 transition"
-      />
-    </div>
-  </Link>
-</div>
+          {/* Envelope */}
+          <Link to="/contact">
+            <div className="relative inline-block w-[30px] h-[30px]">
+              <FaEnvelope
+                size={30}
+                className="absolute top-1/2 left-1/2
+                          transform -translate-x-1/2 -translate-y-1/2
+                          text-black"
+              />
+              <FaEnvelope
+                size={26}
+                className="absolute top-1/2 left-1/2
+                          transform -translate-x-1/2 -translate-y-1/2
+                          text-white hover:text-yellow-500 transition"
+              />
+            </div>
+          </Link>
+        </div>
 
 
         
@@ -106,7 +106,15 @@ const Footer = () => {
             This website is intended for individuals 21 years of age or older. Please drink responsibly. 
             We do not condone excessive or irresponsible alcohol consumption. Always comply with the laws and 
             regulations in your area regarding alcohol use. If you or someone you know struggles with alcohol 
-            dependency, please seek help from a professional organization.
+            dependency, please seek help from a professional organization
+            <a
+              href="https://www.nj.gov/humanservices/reachnj/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block ml-1"
+              >
+              <FaExternalLinkAlt size={12} />
+            </a>.
           </p>
         </div>
 
