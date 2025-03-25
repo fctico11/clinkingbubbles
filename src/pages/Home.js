679ce61e4163248d11332d7a306f0c94ee01ad05
fctico11 <fac322@nyu.edu>
2025-03-25 14:38:32 -0600

    added get a quote button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,12 +24,22 @@ const Home = () => {
       <WhatWeBring />
       <CredentialSection />
       <section className="py-12 text-center">
-        <Link to="/booking-process">
-          <button className="bubbles-font text-lg bg-yellow-500 hover:bg-yellow-600 text-black font-semibold px-6 py-3 rounded-full transition">
-            Learn more about the Booking process
-          </button>
-        </Link>
+        <div className="flex flex-col items-center space-y-4">
+          <Link to="/booking-process">
+            <button className="bubbles-font text-lg bg-black hover:bg-gray-800 text-white font-semibold px-6 py-3 rounded-full transition">
+              Learn more about the Booking process
+            </button>
+          </Link>
+          <Link to="/contact">
+            <button className="bubbles-font text-lg bg-yellow-500 hover:bg-yellow-600 text-black font-semibold px-6 py-3 rounded-full transition">
+              Get a Quote
+            </button>
+          </Link>
+        </div>
       </section>
+
+
+      
       <Footer />
 
       {/* Overlay animation on top of homepage */}
