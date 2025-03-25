4aef50d0c76d8ff5bbfd072f705ce2922cf715de
fctico11 <fac322@nyu.edu>
2025-03-25 17:49:54 -0600

    added react helmet to preload heroimage

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,32 +1,39 @@
 // src/components/HeroSection.js
 import React from "react";
+import { Helmet } from "react-helmet"; // Import React Helmet
 import heroImage from "../assets/images/hero.webp"; // Import image
 import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <section
-      className="relative bg-cover bg-center bg-no-repeat text-white py-32 px-4 text-center"
-      style={{ backgroundImage: `url(${heroImage})` }}
-    >
-      {/* Overlay for Better Text Readability */}
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+    <>
+      <Helmet>
+        <link rel="preload" as="image" href={heroImage} />
+      </Helmet>
+      <section
+        className="relative bg-cover bg-center bg-no-repeat text-white py-32 px-4 text-center"
+        style={{ backgroundImage: `url(${heroImage})` }}
+      >
+        {/* Overlay for Better Text Readability */}
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
-      {/* Content */}
-      <div className="relative z-10 max-w-3xl mx-auto">
-        <h1 className="clinking-font text-4xl md:text-6xl font-bold mb-4">Elevate Your Experience</h1>
-        <p className="bubbles-font text-2xl md:text-2xl mb-8">
-          Private bartending services that dazzle and delight.
-        </p>
-        <Link to="/contact">
-          <button className="bubbles-font text-lg bg-yellow-500 text-black font-semibold px-6 py-3 rounded-full hover:bg-yellow-600 transition">
-            Book Now
-          </button>
-        </Link>
-      </div>
-    </section>
+        {/* Content */}
+        <div className="relative z-10 max-w-3xl mx-auto">
+          <h1 className="clinking-font text-4xl md:text-6xl font-bold mb-4">
+            Elevate Your Experience
+          </h1>
+          <p className="bubbles-font text-2xl md:text-2xl mb-8">
+            Private bartending services that dazzle and delight.
+          </p>
+          <Link to="/contact">
+            <button className="bubbles-font text-lg bg-yellow-500 text-black font-semibold px-6 py-3 rounded-full hover:bg-yellow-600 transition">
+              Book Now
+            </button>
+          </Link>
+        </div>
+      </section>
+    </>
   );
 };
 
 export default HeroSection;
-
