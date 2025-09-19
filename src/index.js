edbb424b2f7dc9f75358b8fb116c93e63ac39c29
fctico11 <64610595+fctico11@users.noreply.github.com>
2025-09-18 21:14:07 -0400

    change to keep meta off critical path

--- src/index.js
+++ src/index.js
@@ -5,39 +5,43 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 
-// Add Meta Pixel base code
-(function(f, b, e, v, n, t, s) {
-  if (f.fbq) return;
-  n = f.fbq = function() {
-    n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-  };
-  if (!f._fbq) f._fbq = n;
-  n.push = n;
-  n.loaded = !0;
-  n.version = '2.0';
-  n.queue = [];
-  t = b.createElement(e);
-  t.async = !0;
-  t.src = 'https://connect.facebook.net/en_US/fbevents.js';
-  s = b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t, s);
-})(window, document, 'script');
+// Defer Facebook Pixel loading until after main content is rendered
+function loadFacebookPixel() {
+  (function(f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function() {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = 'https://connect.facebook.net/en_US/fbevents.js';
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(window, document, 'script');
 
-// Initialize Pixel
-window.fbq('init', '1167680571793485');
-window.fbq('track', 'PageView'); // Track sitewide page views
+  // Initialize Meta Pixel
+  window.fbq('init', '1167680571793485');
+  window.fbq('track', 'PageView');
+}
 
+// Prefer requestIdleCallback, fall back to window.onload
+if ('requestIdleCallback' in window) {
+  requestIdleCallback(loadFacebookPixel);
+} else {
+  window.addEventListener('load', loadFacebookPixel);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HelmetProvider>
-    
-      <App />
-    
+    <App />
   </HelmetProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional performance measurement
 reportWebVitals();
