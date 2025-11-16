adc2bcb59db11c6340bdd76b4685af72c0ec30cb
fctico11 <64610595+fctico11@users.noreply.github.com>
2025-11-16 15:51:16 -0500

    remove preloads

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,20 +40,6 @@ const Home = () => {
         <meta name="twitter:image:alt" content="Private event bartending for weddings and parties in NJ & NY." />
      
         <link rel="preload" href="/assets/champagne.json" as="fetch" type="application/json" crossorigin="anonymous" />
-        <link
-          rel="preload"
-          href="/media/VELISTA.otf"
-          as="font"
-          type="font/otf"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/media/AppleGaramond.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
       </Helmet>
 
       <Navbar />
